7cee334ff162ccf2e3334c44c63685d5f5e5d9f9
guille <guille@DESKTOP-PBFV40H>
2023-12-02 12:10:49 -0400

    Se crea el codigo de abril, y se coloca el msj Este es el codigo de abril

--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,7 @@
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
-        System.out.println("Hello world");
-        System.out.println("Hello world2");
-        System.out.println("El area bajo la curva: 18");
-        System.out.println("El area debajo de la curva: -18");
+        System.out.println("Este es el codigo de Abril");
     }
     
 }
