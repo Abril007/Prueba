2df8eff8868f0b205e4ecdd7b7fb151854791136
guille <guille@DESKTOP-PBFV40H>
2023-12-02 11:48:36 -0400

    Se agrega hello world2

--- src/Principal.java
+++ src/Principal.java
@@ -3,6 +3,8 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Hello world");
+        System.out.println("Hello world2");
+
     }
     
 }
