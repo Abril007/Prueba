932084eeaa9725a92d8f55e9a8d62ba26b9d4522
Abril007 <152710436+Abril007@users.noreply.github.com>
2023-12-02 11:58:34 -0400

    Update Principal.java
    Se agrega el area bajo la curva: 18

--- src/Principal.java
+++ src/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
         System.out.println("Hola mundo");
         System.out.println("Hello world");
         System.out.println("Hello world2");
-
+        System.out.println("El area bajo la curva: 18");
     }
     
 }
